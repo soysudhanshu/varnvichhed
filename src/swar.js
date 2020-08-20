8aef392d92910cc97f3795e78df93638a92d942f
Sudhanshu Rathore <soysudhanshu@gmail.com>
2020-08-20 16:18:02 +0530

    Added matraToSwar function

--- src/swar.js
+++ src/swar.js
@@ -26,6 +26,22 @@ const isSwar = varn => SWARS.some(swar => swar.varn === varn);
  */
 const isMatra = char => SWARS.some(swar => (swar.matra === char && char !== null));
 
+/**
+ * Coverts matra into corresponding swar.
+ * @param {string} matra
+ * @returns {string|null} If no swar is found, then returned `null`.
+ */
+const matraToSwar = matra => {
+    const swar = SWARS.filter(swar => (swar.matra === matra && matra !== null));
+
+    if(swar.length === 0){
+        return null;
+    }
+
+    return swar[0].varn;
+}
+
 module.exports.SWARS = SWARS;
 module.exports.isSwar = isSwar;
 module.exports.isMatra = isMatra;
+module.exports.matraToSwar = matraToSwar;
