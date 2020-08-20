8608c11bfd65c77dc1fd53ed487596acb6ce8e9a
Sudhanshu Rathore <soysudhanshu@gmail.com>
2020-08-20 15:46:36 +0530

    Added tests for isMatra function.

--- tests/swar.test.js
+++ tests/swar.test.js
@@ -1,8 +1,30 @@
 const { viched } = require('../src/varnviched.js');
-const { SWARS } = require('../src/swar.js');
+const { SWARS, isMatra } = require('../src/swar.js');
 
 SWARS.forEach(swar => {
     test(`Do no viched ${swar.varn}`, () => {
         expect(viched(swar.varn)).toStrictEqual([swar.varn]);
+    });
+});
+
+test('Test isMatra()', function () {
+    const matraye = [
+        "ा", "ि", "ी", "ु",
+        "ू", "े", "ै", "ो",
+        "ौ", "ृ"
+    ];
+    
+    matraye.forEach(matra => {
+        expect(isMatra(matra)).toBeTruthy();
+    });
+
+    const notMatraye = [
+        0, null, '', false, true,
+        'a', 'ज्ञ', 'त्र', 'आ',
+        'अ', 'ऐ', '1'
+    ];
+
+    notMatraye.forEach(matra => {
+        expect(isMatra(matra)).toBeFalsy();
     });
 });
