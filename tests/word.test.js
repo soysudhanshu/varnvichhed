7e893184b34f05cdd2cb1a5276a43df95c920bbf
Sudhanshu Rathore <soysudhanshu@gmail.com>
2020-08-22 16:56:34 +0530

    Added tests for मात्रायुक्त शब्द

--- tests/word.test.js
+++ tests/word.test.js
@@ -1,9 +1,16 @@
 const { viched } = require("../src/varnviched");
 
 const {MATRAHIN_SHABD} = require('./data/मात्राहीन-शब्द.js');
+const { MATRAO_WALE_SHABD } = require("./data/मात्रायुक्त-शब्द.js");
 
 test('Vichhed words without matra', () => {
     MATRAHIN_SHABD.forEach(shabd => {
         expect(viched(shabd.sabdh)).toStrictEqual(shabd.vichhed)
     });
+});
+
+test('Vichhed words with matra', ()=> {
+    MATRAO_WALE_SHABD.forEach(shabd => {
+        expect(viched(shabd.shabd)).toStrictEqual(shabd.vichhed);
+    })
 });
